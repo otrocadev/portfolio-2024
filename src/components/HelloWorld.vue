6f8a0b78ba355a20f6a9d8de51ac54665cd5d087
Ot Roca Tanyà <otroca99@MacBook-Air-de-Ot.local>
2024-02-24 10:58:10 +0100

    Adding title

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,10 @@
 <template>
   <div class="hello">
-    <h1>Hello World</h1>
+    <h1>Ot Roca's portfolio</h1>
+    <p>Working on it...</p>
   </div>
 </template>
 
-<script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-};
-</script>
+<script></script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
